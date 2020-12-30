72a7ac7e6ec71dd53f5d8431d9bb0a652eff6a47
tet1q <akuhidupkok@gmail.com>
2020-12-30 21:39:59 +0700

    Hehe

---
+++ src/help.js
@@ -0,0 +1,82 @@
+const help = (prefix) => { 
+	return `                 
+  MENU BOT G GUNA
+  
+ *Sticker*
+ *${prefix}sticker*
+ *${prefix}tsticker*
+ *${prefix}nulis*
+ 
+ *New Update*
+ *${prefix}nulis*
+ *${prefix}text3d*
+ *${prefix}ninjalogo*
+ *${prefix}quotes*
+ *${prefix}lirik*
+ *${prefix}bucin*
+ *${prefix}wolflogo*
+ *${prefix}lionlogo*
+ *${prefix}tebakgambar*
+ *${prefix}caklontong*
+ *${prefix}family100*
+ *${prefix}game*
+ *${prefix}textscreen <teks>*
+ *${prefix}tahta <teks>*
+ *${prefix}rtext <teks>*
+ *${prefix}glitch <teks> | <teks>*
+ *${prefix}party <teks>*
+ *${prefix}lovemake <teks>*
+ *${prefix}primbonjodoh <teks> | <teks>*
+ *${prefix}artinama <nama>*
+ *${prefix}ramalhp <nomor>*
+
+ *Nsfw menu*
+ *${prefix}loli*
+ *${prefix}waifu*
+ *${prefix}randomhentai*
+ *${prefix}nsfwtrap*
+ *${prefix}nsfwloli*
+ *${prefix}nsfwneko*
+ *${prefix}loli*
+Kalo mau mengaktifkan nya ketik
+nsfw1 kalo mau di nonaktifkan nsfw0
+
+ *Download Menu*
+ *ytsearch* [search yt]
+ *ytmp3* [link]
+ *tiktok* [link]
+ 
+ *Group Only*
+ *${prefix}add* [62xxx]
+ *${prefix}kick* [tag]
+ *${prefix}setpp*
+ *${prefix}demote* [tag]
+ *${prefix}promote* [tag]
+ *${prefix}setpp*
+ *${prefix}group* [buka/tutup]
+ *${prefix}welcome* [1/0]
+ *${prefix}nsfw* [1/0]
+ *${prefix}simih* [1/0]
+ *${prefix}groupinfo*
+
+ *Owners*
+ *${prefix}bc* 
+ *${prefix}leave*
+ *${prefix}clearall*
+ *${prefix}setprefix*
+ *${prefix}clone* [tag]
+ *${prefix}block*
+ *${prefix}unblock*
+ *${prefix}getses*
+ 
+*Others*
+ *${prefix}ytsearch*
+ *${prefix}listadmin*
+ *${prefix}blocklist*
+ *${prefix}simi*
+ *${prefix}wait*
+ *${prefix}fitnah*
+ *${prefix}tiktokstalk*
+ *${prefix}url2img*`
+}
+exports.help = help
